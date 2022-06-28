27760e04ab4df1248545b8c1c422b769c105c319
LacErnest <laceinstein@gmail.com>
2022-06-28 08:59:12 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  console.log('Welcome')
+
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
